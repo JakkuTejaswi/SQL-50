3f14ebafe03a75a0ecb7ef383d216e4cfb8e8e5e
JakkuTejaswi <tejaswijakku71511@gmail.com>
2026-06-09 12:35:03 +0530

    solved problems on advanced select and joins

---
+++ 1164 Product Price at a Given Date.sql
@@ -0,0 +1,19 @@
+with cte as(
+    select product_id, new_price , 
+    change_date,
+    row_number()over(partition by product_id
+    order by change_date desc) as rn
+    from Products 
+    where change_date<='2019-08-16'
+)
+select product_id, new_price as price
+from cte 
+where rn=1
+union 
+select product_id, 10 as price 
+from Products 
+where product_id not in(
+    select product_id 
+    from Products 
+    where change_date<='2019-08-16'
+);
